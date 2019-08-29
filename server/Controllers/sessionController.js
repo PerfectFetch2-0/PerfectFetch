2cbe6ecc766c9cd490ed50bfbf74613c2b1786ac
Michelle Herrera <mesherrera@aol.com>
2019-08-29 18:18:37 -0400

    Minor format changes

--- server/Controllers/sessionController.js
+++ server/Controllers/sessionController.js
@@ -3,6 +3,7 @@ const store = require("../Model/sessionStore.js");
 const pgStore = require("connect-pg-simple")(session);
 const sessionController = {};
 
+// Controller responsible for creating a session at login or during sign up.
 sessionController.createSession = (req, res, next) => {
   // ! Error Check: Failure to access createSession correctly.
   req.session.username = res.locals.username;
@@ -12,6 +13,7 @@ sessionController.createSession = (req, res, next) => {
   next();
 };
 
+// Controller responsible for verifying if the session ID on the request is valid.
 sessionController.verifySession = (req, res, next) => {
   store.get(req.session.id, (error, session) => {
     // ! Error Check: Unable to find provided session id in the database.
@@ -19,13 +21,10 @@ sessionController.verifySession = (req, res, next) => {
       console.log("The user did not have a valid session ID.");
       res.status(400).send("Please log in!");
     }
+    // * Error Check Passed: User will be redirected to the main page of the app.
     console.log("The session has been successfully verified!");
     next();
   });
 };
 
-sessionController.test = (req, res, next) => {
-  console.log(req.session);
-  next();
-};
 module.exports = sessionController;
