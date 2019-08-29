f6a4f44957d78beab7f5920698b15949d87d5a04
David Chai <davidchaidev@gmail.com>
2019-08-29 17:07:27 -0400

    frontend changes

---
+++ client/test/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Login from '../components/Login';
+
+describe('Login component', () => {
+  it('Should render one text input and one password input', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find("input[type='text']")).toHaveLength(1);
+    expect(wrapper.find("input[type='password']")).toHaveLength(1);
+  });
+  it('Should render one')
+
+});
